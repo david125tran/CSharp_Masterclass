8875d39d6194592985bd7c58759c8e53ede75b25
David Tran <109535790+david125tran@users.noreply.github.com>
2024-12-26 12:05:33 -0500

    Add files via upload

---
+++ Section15.2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Part_B___To_Do_List_GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
